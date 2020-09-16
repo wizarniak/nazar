4a2018c0d6eab595440c559be76f486df373b015
Qurat <dragon.ace2020@gmail.com>
2020-09-16 19:23:34 +0400

    Send notifications at regular intervals

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-new Notification('Check-in', {
-  'body': 'What are you doing right now?',
-});
+const INTERVAL = 60 * 60 * 1000;
+
+const notify = async () => {
+  new Notification('Check-in', {
+    'body': 'What are you doing right now?',
+  });
+}
+
+const timer = (ms) => {
+  return new Promise(res => setTimeout(res, ms));
+}
+
+const run = async () => {
+  while (true) {
+    notify();
+    await timer(INTERVAL);
+  }
+}
+
+run();
